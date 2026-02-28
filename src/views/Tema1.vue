2e4bf451bb37783c296fb180826a552448ebd8d5
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-02-27 19:25:08 -0500

    responsive tema 1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -6,14 +6,14 @@
       .titulo-principal__numero
         span 1
       h1 Servicios de red
-    .row.justify-content-center.mb-5
+    .row.justify-content-center.mb-5.align-items-center
       .col-lg-7.mb-lg-0.mb-3(data-aos="fade-right")
         p.mb-4(data-aos='fade-right') Cuando se habla de servicios de red, se hace referencia a aquellos que se basan en una red de trabajo, los cuales son instalados en el <em>firewall</em> del servidor. Estos servicios son configurables, para así mantener la transmisión de los datos y su seguridad, brindando ayuda en la simplificación de muchos procesos y problemas, ayudando a que la red funcione eficazmente. (Philippe, 2010).
 
         .cajon.color-acento-contenido.p-4.mb-4(style="background-color: #fff4da")
           p.mb-0 La arquitectura de los servicios de red es fundamentalmente la de cliente-servidor, y en algunos de estos servicios de red, el cliente puede configurar de forma automática la gestión del servicio. Dentro del servicio de red más utilizado está el de (DHCP) Protocolo de Configuración Dinámica de <em>Host</em>. Este es un estándar diseñado para reducir la carga administrativa y la complejidad de la configuración de <em>hosts</em> en un Protocolo de Control de Transmisión / Protocolo de Internet (TCP/IP) basado en red.
         p.mb-5(data-aos='fade-right') Se puede gestionar la configuración TCP/IP en los equipos clientes de forma automática en el protocolo DHCP con relación al siguiente esquema.
-      .col-lg-5.col-8(data-aos="fade-left")
+      .col-lg-5(data-aos="fade-left")
         .titulo-sexto.color-acento-contenido(data-aos='fade-right')
           h5 Figura 1
           br
@@ -57,10 +57,11 @@
       .col-lg-5.col-8(data-aos="fade-left"): img(src='@/assets/curso/temas/tema1/2.svg', alt='')
 
     .row.mt-5
-      h3 Acuerdos de nivel de servicio
+      h3.mb-4 Acuerdos de nivel de servicio
 
-    .row.justify-content-center.mb-5
-      .col-lg-5.col-8(data-aos="fade-right"): img(src='@/assets/curso/temas/tema1/3.svg', alt='')
+    .row.justify-content-center.mb-5.align-items-center
+      .col-lg-5.col-8(data-aos="fade-right")
+        img.mb-4.mb-lg-0(src='@/assets/curso/temas/tema1/3.svg', alt='')
       .col-lg-7(data-aos="fade-left")
         p Es importante que en todo servicio se elabore un documento o contrato que contemple los acuerdos y condiciones que deben cumplir el proveedor y el cliente; a esto se le llama acuerdo del nivel de servicio SLA (<em>service level agreement</em>); el cual es importante para garantizar que el servicio prestado se cumpla entre ambas partes. En el área TIC se les llama SLA; con este el proveedor se compromete con el cumplimiento de los requerimientos del cliente; estos servicios se organizan por niveles o métricas que indican un rango.
         .cajon.color-primario.p-4.mb-4(style="background-color: #dae4ef")
@@ -70,7 +71,7 @@
     .row.mt-5
       h3 Calidad del servicio
 
-    .row.justify-content-center.mb-5
+    .row.justify-content-center.mb-5.align-items-center
       .col-lg-7(data-aos="fade-right")
         p El término de calidad del servicio se refiere a la capacidad de una red para brindar los distintos niveles de garantías al servicio en el tráfico; ahora bien, el QoS es una técnica que mejora el uso de la red en donde se priorizará el tráfico.
         .cajon.color-acento-contenido.p-4.mb-4(style="background-color: #ffecc1")
@@ -86,7 +87,7 @@
           li.d-flex
             i.fas.fa-circle.fa-xs(style='color: #00defc')
             | Seguridad.
-      .col-lg-5.col-8(data-aos="fade-left")
+      .col-lg-5(data-aos="fade-left")
         .titulo-sexto.color-acento-contenido(data-aos='fade-right')
           h5 Figura 2
           br
@@ -98,37 +99,37 @@
 
     p.mb-5(data-aos='fade-right') Podemos definir que los servicios de redes son un medio en donde dos o más sistemas se comunican; estas tienen una arquitectura que generalmente es la de cliente-servidor, en donde se exponen las tareas que el cliente puede consumir. A continuación, se encuentran los tipos de servicios en red:
     .contenedor-tarjetas.d-flex.flex-wrap.justify-content-center
-        .col-md.tarjeta.p-4.m-1(style="background-color: #dae4ef")
+        .col-md-4.col-lg-3.col-xl.tarjeta.p-4.m-1(style="background-color: #dae4ef")
           .row.justify-content-center.mb-4
-            .col-7
+            .col-4.col-md-6
               figure
                 img(src='@/assets/curso/temas/tema1/5.svg', alt='')
           h4.text-center Impresión
           p Este servicio nos permite compartir impresoras en una red.
-        .col-md.tarjeta.p-4.m-1(style="background-color: #dae4ef")
+        .col-md-4.col-lg-3.col-xl.tarjeta.p-4.m-1(style="background-color: #dae4ef")
           .row.justify-content-center.mb-4
-            .col-7
+            .col-4.col-md-6
               figure
                 img(src='@/assets/curso/temas/tema1/6.svg', alt='')
           h4.text-center Comunicación
           p Permite comunicar a varios usuarios utilizando mensajes (<em>e-mail</em>).
-        .col-md.tarjeta.p-4.m-1(style="background-color: #dae4ef")
+        .col-md-4.col-lg-3.col-xl.tarjeta.p-4.m-1(style="background-color: #dae4ef")
           .row.justify-content-center.mb-4
-            .col-7
+            .col-4.col-md-6
               figure
                 img(src='@/assets/curso/temas/tema1/7.svg', alt='')
           h4.text-center Información
           p Aquí los servidores almacenan información como lo son las bases de datos para que sean consultadas por un usuario (<em>web</em>).
-        .col-md.tarjeta.p-4.m-1(style="background-color: #dae4ef")
+        .col-md-4.col-lg-3.col-xl.tarjeta.p-4.m-1(style="background-color: #dae4ef")
           .row.justify-content-center.mb-4
-            .col-7
+            .col-4.col-md-6
               figure
                 img(src='@/assets/curso/temas/tema1/8.svg', alt='')
           h4.text-center Ficheros
           p Este servicio es ideal para guardar grandes volúmenes de información para que el cliente pueda hacer la descarga (FTP).
-        .col-md.tarjeta.p-4.m-1(style="background-color: #dae4ef")
+        .col-md-4.col-lg-3.col-xl.tarjeta.p-4.m-1(style="background-color: #dae4ef")
           .row.justify-content-center.mb-4
-            .col-7
+            .col-4.col-md-6
               figure
                 img(src='@/assets/curso/temas/tema1/9.svg', alt='')
           h4.text-center Administración
@@ -137,12 +138,12 @@
     .row.mt-5
       h3 Protocolos y servicios de red
 
-    .cajon-b.color-secundario.p-3.mb-5
+    .cajon-b.color-secundario.p-3.mb-lg-4
       p.mb-0 Los protocolos se definen como un conjunto de reglas permanentes en la red; estos protocolos son estándares y tienen políticas que definen la comunicación entre dos o más equipos en la red; estas políticas aseguran la comunicación de extremo a extremo y también los protocolos que deben ser instalados por el emisor y receptor, para dar garantía a la comunicación de la red. (Philippe, 2010).
       br
       p Ahora bien, cuando se hace referencia a una pila de protocolos, se está hablando del conjunto completo de capas que trabajan entre sí para brindar capacidades de red.
 
-    .row.justify-content-center.mb-5
+    .row.justify-content-center.mb-5.align-items-center
       .col-lg-7(data-aos="fade-right")
         p Dentro de los protocolos más comunes, se encuentra el protocolo HTTP (protocolo de transferencia de hipertexto), que es la base de la comunicación de datos en la internet; también se encuentra el HTTPS (protocolo de transferencia de hipertexto sobre SSL/TLS), que utiliza los mismos servicios del HTTP, pero utilizando una conexión segura.
           br
@@ -150,15 +151,15 @@
           |FTP (protocolo de transferencia de archivos) es utilizado para transferir datos en internet y en redes privadas; SSH (<em>shell</em> seguro) se utiliza para administrar dispositivos en una red de forma segura; SMTP (protocolo simple de transferencia de correo) se utiliza para enviar mensajes o datos desde el origen al destino entre servidores de correo.
         .cajon.color-acento-contenido.p-4.mb-4(style="background-color: #ffecc1")
           p.mb-0 Otro protocolo de importancia es el DNS (sistema de nombres de dominio), que se utiliza para convertir el nombre en una dirección IP; este utiliza el puerto 53.
-      .col-lg-5.col-8(data-aos="fade-left")
+      .col-lg-5(data-aos="fade-left")
         .titulo-sexto.color-acento-contenido(data-aos='fade-right')
           h5 Figura 3
           br
           span <em>Protocolos de red</em>
         img(src='@/assets/curso/temas/tema1/10.svg', alt='Protocolos de red. Nombre de la capa: - Aplicación. - Transporte. - Internet. - Acceso a la red. Pila de productos: - Protocolo de transferencia de hipertexto (HTTP). - Protocolo de control de transmisión (TCP). - Protocolo de internet (IP). - Ethernet.')
-    .row.justify-content-center
-      .col-lg-2.col-8(data-aos="fade-right")
-        img(src='@/assets/curso/temas/tema1/11.jpg', alt='')
+    .row.justify-content-center.align-items-center
+      .col-lg-2.col-4(data-aos="fade-right")
+        img.mb-4.mb-lg-0(src='@/assets/curso/temas/tema1/11.jpg', alt='')
       .col-lg-10(data-aos="fade-left")
         p Ahora bien, con relación a los servicios de red, se resalta que estos facilitan la conexión entre dos o más ordenadores, dando como finalidad compartir recursos tanto en el <em>software</em> como en el <em>hardware</em>; es por esto que podemos compartir impresoras o programas en una red, así como también los servicios de red garantizan una mejor seguridad de la información.
 
